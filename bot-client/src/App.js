78cb0d8732be3a02d6ffc43ddbdd3369874b0cab
anna baek <yeon376@yonsei.ac.kr>
2022-02-20 17:57:11 +0900

    gauge component - tailwind css 수정 예정

--- bot-client/src/App.js
+++ bot-client/src/App.js
@@ -1,12 +1,11 @@
 import RageProvider from "./context/RageContext";
+import Gauge from "./components/Gauge";
 
 function App() {
   return (
     <RageProvider>
       <div className="App">
-        {/**
-         *
-         */}
+        <Gauge  />
       </div>
     </RageProvider>
   );
