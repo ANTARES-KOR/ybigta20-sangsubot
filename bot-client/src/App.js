2e8156e990f23043818c6fd8e6f96bf5c01893f6
ANTARES_SEO <75557859+ANTARES-KOR@users.noreply.github.com>
2022-02-20 15:32:27 +0900

    frontend base model

---
+++ bot-client/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import RageProvider from "./context/RageContext";
+
+function App() {
+  return (
+    <RageProvider>
+      <div className="App">
+        {/**
+         *
+         */}
+      </div>
+    </RageProvider>
+  );
+}
+
+export default App;
