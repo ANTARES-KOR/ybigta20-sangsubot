53c3983a561ad54f574415fd5ecc9b164baa9ef6
nicole2kim <nicole2kim@yonsei.ac.kr>
2022-02-24 19:52:23 +0900

    photo edit

--- bot-client/src/components/Viewer.jsx
+++ bot-client/src/components/Viewer.jsx
@@ -1,11 +1,11 @@
 import { useRageContext } from "../context/RageContext";
 import '../main.css';
-import 사진1 from './img/사진1.png';
+import 사진1 from './img/사진1.gif';
 import 사진2 from './img/사진2.png';
 import 사진3 from './img/사진3.PNG';
 import 사진4 from './img/사진4.png';
 import 사진5 from './img/사진5.png';
-import 사진6 from './img/사진6.png';
+import 사진6 from './img/사진6.gif';
 
 export default function Viewer() {
     const rage = useRageContext();
