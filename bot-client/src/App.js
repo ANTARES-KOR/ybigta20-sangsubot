c82c62d89f12937fba5dd513a7e05633ef9eda90
nicole2kim <nicole2kim@yonsei.ac.kr>
2022-02-24 16:55:01 +0900

    viewer, audio edit

--- bot-client/src/App.js
+++ bot-client/src/App.js
@@ -1,6 +1,7 @@
 import RageProvider from "./context/RageContext";
 import Gauge from "./components/Gauge";
 import Viewer from "./components/Viewer";
+import PlayAudio from "./components/Audio";
 import Form from "./components/Form";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="App" style={{marginTop: '15%', display: "flex", justifyContent: 'center', alignItems: "center", flexDirection: 'column'}}>
         <Gauge  />
         <Viewer />
+        <PlayAudio />
         <Form />
       </div>
     </RageProvider>
